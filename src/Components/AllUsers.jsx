149baa4e038678cf16982b09a34ac755bb5f693c
saurabh <saurabhkedar@gmail.com>
2021-12-03 09:41:10 +0530

    moving to master

--- src/Components/AllUsers.jsx
+++ src/Components/AllUsers.jsx
@@ -1,4 +1,17 @@
-const AllUsers = () => {
+import { getUsers } from "../Service/api";
+
+const AllUsers = async () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = async () => {
+    const response = await getUsers();
+    console.log(response.data);
+    setUsers(response.data);
+  };
   return <p>Hello from all</p>;
 };
 
